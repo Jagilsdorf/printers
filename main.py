8f4e160f1731a7b70a8e51bb21ae0ff6aa6683ef
jagilsdorf <jagilsdorf@gmail.com>
2023-10-12 12:26:13 -0400

    fixed powershell printer query

--- main.py
+++ main.py
@@ -2,11 +2,10 @@ import csv, subprocess, os, json; from datetime import datetime
 
 if 'OPTIONS' == 'OPTIONS':
     printing =      True    #Default: False | 
-    update_json =   True   #Default: False | If true, printer.json will be deleted and replaced
+    update_json =   False   #Default: False | If true, printer.json will be deleted and replaced
 
 if 'CONSTANTS' == 'CONSTANTS':   
-    if datetime.now().strftime('%A') == 'Monday' or os.path.exists('printer.json') == False:
-        update_json = True
+    if datetime.now().day == 1 or os.path.exists('printer.json') == False: update_json = True
     all_printers = {}
 
 if "FUNC" == "FUNC":
@@ -24,35 +23,25 @@ if "FUNC" == "FUNC":
             with open('printer.json', 'r') as file: dict = json.load(file)
         return dict
 
-    def get_ip_from_hostname_powershell(hostname):
-        try:
-            command = f"Resolve-DnsName {hostname}"
-            output = subprocess.check_output(['powershell', '-Command', command], stderr=subprocess.STDOUT, text=True)
-            for line in output.splitlines():
-                if hostname in line and " A " in line:  # Ensure we are picking up only the IPv4 records
-                    return line.split()[-1]
-        except subprocess.CalledProcessError:
-            pass
-        return None
-
-    def update_printer_DNS():
-        all_printers = {'DEP': {}, 'KAN': {}, 'ONT': {}}
-
-        for x in range(1, 256):  # Include ABC1P001 to ABC1P255
-            formatted_name = f"{x:03}"  # Formats the number to 3 digits with leading zeros
-            all_printers['DEP'][f'DEP1P{formatted_name}'] = {}
-            all_printers['KAN'][f'KAN1P{formatted_name}'] = {}
-            all_printers['ONT'][f'ONT1P{formatted_name}'] = {}
-
-        for location in list(all_printers.keys()):
-            for printer in list(all_printers[location].keys()):
-                all_printers[location][printer] = {'ip':get_ip_from_hostname_powershell(printer)}
-                json_rw('save',all_printers)
-
+    def get_printers_from_UNC(UNC_name):
+        output = subprocess.check_output(['powershell', '-Command', f"Get-Printer -ComputerName {UNC_name}"], stderr=subprocess.STDOUT, text=True)
+        temp = {}
+        for line in output.splitlines():
+            if not line.strip() or line.startswith("Name"):
+                continue
+            else:
+                location, hostname, ip = line.split()[0][:3], line.split()[0], line.split()[-4]
+                print(location, hostname, ip)
+                if ip.startswith('10.1'):
+                    print(hostname)
+                    if location not in list(temp.keys()): temp[location] = {}
+                    if hostname not in list((temp[location]).keys()): temp[location][hostname] = {}
+                    temp[location][hostname]['ip']= ip
+                    json_rw('save',temp)
+                    print('\n\n\n\n\n\n\n\n\n\n\n\n')
+        return temp
 
 if 'INITIALIZE' == 'INITIALIZE':
-    #Update json if needed & load into all_variable
-    if update_json == True: update_printer_DNS()
+    if update_json == True: all_printers = (get_printers_from_UNC("print-us"))
     else: all_printers = json_rw('open', all_printers)
-
-
+    
