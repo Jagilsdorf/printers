2afc6b1db09b37ad26fdaf22301791d0ce2d55d3
jagilsdorf <jagilsdorf@gmail.com>
2023-10-12 10:46:35 -0400

    fixed nested dict & auto update

--- main.py
+++ main.py
@@ -2,25 +2,26 @@ import csv, subprocess, os, json; from datetime import datetime
 
 if 'OPTIONS' == 'OPTIONS':
     printing =      True    #Default: False | 
-    update_json =   False   #Default: False | If true, printer.json will be deleted and replaced
+    update_json =   True   #Default: False | If true, printer.json will be deleted and replaced
 
 if 'CONSTANTS' == 'CONSTANTS':   
     if datetime.now().strftime('%A') == 'Monday' or os.path.exists('printer.json') == False:
         update_json = True
+    all_printers = {}
 
 if "FUNC" == "FUNC":
-    def csv_write(path, data):
+    def csv_write(path, headers, data):
         with open(path, mode='w', newline='') as file:
             writer = csv.writer(file)
-            writer.writerow(["Name", "IP Address", "Model Name", "Published in AD"])
+            writer.writerow(headers)
             writer.writerows(data)
 
-    def func(save_or_open, dict):
-        dict = {}
+    def json_rw(save_or_open, dict):
         if save_or_open == "save":
-                with open('printer.json', 'w') as file: json.dump(dict, file)
+            with open('printer.json', 'w') as file: json.dump(dict, file)
         else:
-                with open('printer.json', 'r') as file: dict = json.load(file)
+            dict = {}
+            with open('printer.json', 'r') as file: dict = json.load(file)
         return dict
 
     def get_ip_from_hostname_powershell(hostname):
@@ -39,19 +40,19 @@ if "FUNC" == "FUNC":
 
         for x in range(1, 256):  # Include ABC1P001 to ABC1P255
             formatted_name = f"{x:03}"  # Formats the number to 3 digits with leading zeros
-            all_printers['DEP'][f'DEP1P{formatted_name}'] = None
-            all_printers['KAN'][f'KAN1P{formatted_name}'] = None
-            all_printers['ONT'][f'ONT1P{formatted_name}'] = None
-
+            all_printers['DEP'][f'DEP1P{formatted_name}'] = {}
+            all_printers['KAN'][f'KAN1P{formatted_name}'] = {}
+            all_printers['ONT'][f'ONT1P{formatted_name}'] = {}
 
         for location in list(all_printers.keys()):
-            for printer_name in all_printers[location]:
-                ip_address = get_ip_from_hostname_powershell(printer_name)
-                if ip_address:
-                    print(f"{printer_name} -> {ip_address}")
-                else:
-                    print(f"{printer_name} could not be resolved.")
+            for printer in list(all_printers[location].keys()):
+                all_printers[location][printer] = {'ip':get_ip_from_hostname_powershell(printer)}
+                json_rw('save',all_printers)
 
 
 if 'INITIALIZE' == 'INITIALIZE':
+    #Update json if needed & load into all_variable
     if update_json == True: update_printer_DNS()
+    else: all_printers = json_rw('open', all_printers)
+
+
